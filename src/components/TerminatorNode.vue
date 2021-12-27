77e6a065981d3a9fc0f333c9860c9e788c61a897
georgius1024 <georgius@ural.ru>
2021-12-27 23:04:49 +0500

    non-interactive terminator node

--- src/components/TerminatorNode.vue
+++ src/components/TerminatorNode.vue
@@ -1,32 +1,12 @@
 <template>
-  <div
-    class="generic-node end-node"
-    :class="{ 'drag-over': dragOver, rejected }"
-    :style="terminatorStyle"
-    @click.stop="click"
-    @mousedown.stop
-  >
-    <div
-      class="dropzone"
-      @drop.stop="drop"
-      @dragend="dragEnd"
-      @dragover.prevent
-      @dragenter.prevent="dragenter"
-      @dragleave.prevent="dragleave"
-    />
-    <img :src="plus" class="icon" />
-  </div>
+  <div class="generic-node end-node" :style="terminatorStyle">END</div>
 </template>
 <script>
 import BaseNode from './BaseNode.vue';
-import plus from '../assets/icons/plus.svg';
 
 export default {
   extends: BaseNode,
   computed: {
-    plus() {
-      return plus;
-    },
     size() {
       return this.height / 2;
     },
@@ -41,15 +21,6 @@ export default {
         };
       }
     }
-  },
-  methods: {
-    drop(event) {
-      this.dragOver = false;
-      const from = event.dataTransfer.getData('id');
-      const to = this.parent;
-      const left = ['stop-left', 'stop'].includes(this.type);
-      this.$emit('dropOn', { from, to, left });
-    }
   }
 };
 </script>
@@ -61,14 +32,8 @@ export default {
   justify-content: center;
   pointer-events: none;
   &.drag-over {
-    transform: scale(1.5);
-    opacity: 0.5;
-  }
-  .dropzone {
-    position: absolute;
-    inset: -50px;
-    transition: all 200ms ease;
-    pointer-events: auto;
+    transform: scale(1);
+    opacity: 1;
   }
 }
 </style>
